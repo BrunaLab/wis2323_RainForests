a552e7153a42c22ae8fc4494f1e0ff0818927df2
embruna <embruna@ufl.edu>
2023-08-01 11:01:14 -0400

    updated icons, updates slos, course policies

--- class_materials/syllabus/course_schedule/png_generator.R
+++ class_materials/syllabus/course_schedule/png_generator.R
@@ -1,7 +1,19 @@
+library(fontawesome)
+# thumbs-up
+# headphones
+# scale-balanced
+# person-breastfeeding
+# microphone
+# computer
+# person-chalkboard
+#forestgreen
+
 if (interactive()) {
-  
   # Create a Font Awesome SVG icon as a
   # PNG file on disk
-  fa_png(name = "r-project")
+  
+  fa_png(name = "computer",
+         file="./class_materials/syllabus/course_schedule/icons/computer-wt.png",
+         fill="white")
   
 }
