2a35ac75f89b437ba386d048eadc5d3cb762f28a
embruna <embruna@ufl.edu>
2023-08-17 18:52:00 -0400

    UPDATES SYLLABUS FORMATTING AND CONTENT

--- class_materials/syllabus/syllabus_sections/assignments_plot.R
+++ class_materials/syllabus/syllabus_sections/assignments_plot.R
@@ -1,54 +1,57 @@
-# library
-library(ggplot2)
-
-# create a dataset
-semester <- c("Assignments")
-tasks <- c("In-class\nActivities" , "Movie\nReview" ,
-           "Analytic\nEssay","Reflective\nEssay","Final\nProject")
-points <- c(400,100,150,150,200)
-data <- data.frame(semester,tasks,points)
-data<-as_tibble(data) %>% 
+data <- read_csv("./class_materials/syllabus/syllabus_sections/assignments.csv") %>%
+# data <- read_csv("./assignments.csv") %>%
+  mutate(semester="Assignments") %>% 
+  rename(tasks=Assignment) %>% 
   mutate(tasks=as.factor(tasks),
          semester=as.factor(semester)
-         ) %>% 
-  arrange(points) %>%    # First sort by val. This sort the dataframe but NOT the factor levels
+  ) %>% 
+  arrange(Points) %>%    # First sort by val. This sort the dataframe but NOT the factor levels
   mutate(tasks=factor(tasks, levels=tasks)) %>% 
-  mutate(perc=round(points/sum(points)*100),2) %>% 
-  mutate(barlabel=paste(tasks,points,sep="\n")) %>% 
+  mutate(perc=round(Points/sum(Points)*100),2) %>% 
+  mutate(barlabel=paste(tasks,Points,sep="\n")) %>% 
   mutate(barlabel=paste(barlabel,"pts",sep=" ")) %>% 
-  mutate(barlabel=paste(barlabel,"\n(", perc,"%)", sep="")) 
+  mutate(barlabel=paste(barlabel," (", perc,"%)", sep="")) %>% 
+  mutate(barlabel=paste(barlabel,"\n", Requirements,sep=""))
 
-# # Stacked + percent
-# ggplot(data, aes(fill=tasks, y=points, x=semester)) + 
-#   geom_bar(position="fill", stat="identity")
+total_pts<-sum(data$Points)
 
 library(RColorBrewer)
 library("viridis")   
 # Stacked
-hw_plot<-ggplot(data, 
-       aes(fill=tasks, label = barlabel, y=points, x=semester)) + 
-  scale_colour_brewer(palette = "YlOrRd", direction = - 1) + 
-  scale_fill_brewer(palette = "Paired", direction=-1)+
+
+color_count<-nrow(data)
+my.cols<-brewer.pal(color_count+4,"YlGn")
+my.cols<-my.cols[color_count:(color_count+4)]
+
+
+ggplot(data, 
+       aes(fill=tasks, label = barlabel, y=Points, x=tasks)) + 
+  # scale_colour_brewer(palette = "YlOrRd", direction = - 1) + 
+  # scale_colour_brewer(palette = "YlGn", direction = - 1) + 
+  scale_fill_manual(values = my.cols)+
+  # scale_fill_brewer(palette = "YlGn", direction=-1)+
+  # labs(x = NULL, y = "Points",title = paste("Assignments (",total_pts," points total)", sep="")) +
   # scale_fill_viridis(discrete = T, option="D") +
   geom_bar(position="stack", stat="identity") +
-  geom_text(size = 6, position = position_stack(vjust = 0.5),fontface="bold", color="white")+
+  geom_text(size = 3.5, 
+            position = position_stack(vjust = 0.5),
+            fontface="bold", 
+            color="white")+
+  scale_y_continuous(limits = c(0,501), expand = c(0, 0))+
   coord_flip()+
-  # geom_col(width = 0.5)+
-  # annotate("text", 
-  #          x=1.4, 
-  #          y=3, 
-  #          label= paste("Total course points:",sum(data$points),sep = " "),
-  #          fontface="bold",
-  #          size=8,
-  #          color="white",
-  #          hjust="left") + 
-  # theme_classic()+
-  theme_void()+
+  theme_classic()+
   # theme(plot.margin = unit(c(0,4,0,0), "pt")) +
-  theme(legend.position="none") 
+  theme(legend.position="none") +
+  theme(axis.text.y=element_blank())+
+  theme(axis.title.y=element_blank())+
+  theme(plot.title = element_text(face = "bold"))+
+  theme(plot.title = element_text(size= 15))+
+  theme(axis.ticks.y=element_blank())+
+  theme(axis.text.x = element_text(face = "bold"))+
+  theme(axis.text.x = element_text(size = 12))+
+  theme(axis.title.x = element_text(face = "bold"))+
+  theme(axis.title.x = element_text(size = 12))+
+  theme(plot.title = element_text(size = 22))+
+  theme(plot.title = element_text(hjust = 0.5)) 
 
-ggsave("./class_materials/syllabus/course_schedule/icons/hw.png", width = 30, height = 5, units="cm")
-
-
-
-
+ggsave("./class_materials/syllabus/syllabus_sections/icons/hw.png", width = 8, height = 4.5, units="in")
